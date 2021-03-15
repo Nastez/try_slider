5f886b8397f6ec949a60c43e373acd3c31dc12a2
Nastez <mineevanastez@gmail.com>
2021-03-15 11:19:14 +0700

    Added

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+	CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
 module.exports = {
 	mode: 'development',
-	entry: {
-		app: './src/index.js',
-		print: './src/print.js',
-	},
+	entry: './src/index.ts',
 	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+	},
 	plugins: [
-		new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+		new CleanWebpackPlugin({
+			cleanStaleWebpackAssets: false
+		}),
 		new HtmlWebpackPlugin({
 			title: 'Output Management',
 		}),
 	],
 	output: {
-		filename: '[name].bundle.js',
+		filename: 'bundle.js',
+		
 		path: path.resolve(__dirname, 'dist'),
+		
 	},
+
 	module: {
 		rules: [{
-			test: /\.css$/,
-			use: [
-				'style-loader',
-				'css-loader',
-			],
-		}, ],
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
 	},
 };
